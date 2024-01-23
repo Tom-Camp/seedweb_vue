94c680d38ef717bbed7a7728f7c1751b65d28626
Tom Camp <tomcamp@protonmail.ch>
2024-01-23 07:20:58 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <main>
+    <ProjectAccordion
+        v-for="project in projects"
+        :key="project.id"
+        :project="project"
+    />
+  </main>
+</template>
+
+<script>
+import ProjectAccordion from "@/components/ProjectAccordion.vue";
+import axios from "axios";
+
+export default {
+  name: "app",
+  components: {
+    ProjectAccordion
+  },
+  data () {
+    return {
+      projects: null
+    }
+  },
+  mounted () {
+    axios
+      .get('http://localhost:5000/api/')
+      .then(response => (this.projects = response.data))
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ProjectAccordion.vue
@@ -0,0 +1,48 @@
+<template>
+  <details class="accordion">
+    <summary class="accordion-header">
+      <hgroup>
+        <h4>{{ project.name }}: {{ project.bed_id }}</h4>
+        <p>{{ project.updated }}</p>
+      </hgroup>
+    </summary>
+    <div class="accordion-body">
+      {{ project.description }}
+      <table role="grid">
+        <thead>
+          <tr>
+            <td></td>
+            <td>Air Temp.</td>
+            <td>Humidity</td>
+            <td>Soil Temp.</td>
+            <td>Soil Moisture</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="data in project.data">
+            <td>{{ data.created_date }}</td>
+            <td>{{ data.sensor_data.air_temp }}&deg; F</td>
+            <td>{{ data.sensor_data.humidity }}%</td>
+            <td>{{ data.sensor_data.soil_temp }}&deg; F</td>
+            <td>{{ data.sensor_data.moisture }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </details>
+</template>
+
+<script>
+export default {
+  props: {
+    project: {
+      type: Object,
+      required: true,
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
